6813848a73e2a2baefd456ec42abf7c0b8cd116f
isman77 <105292650+isman77@users.noreply.github.com>
2022-05-10 15:13:27 +0200

    Rename password.ps1 to PowerShell/ActiveDirectory/password.ps1

---
+++ PowerShell/ActiveDirectory/password.ps1
@@ -0,0 +1,16 @@
+﻿Function Get-MdpGen {
+    for ($i=0;$i -lt 6;$i++){
+
+    $lettre += ("a","b","c","d","e","f","g","h","i","j","k","l") | Get-Random
+
+    }
+    $nombre = Get-Random -Minimum 1 -Maximum 100
+    $caracspec = ("`$","@","&","!") | Get-Random
+    $pasString = $lettre+$nombre+$caracspec
+    #Ajouter ici une commande d'export du mot de passe vers un fichier par exemple
+    $lettre = $null
+    return $pasString
+}
+
+$pass = Get-MdpGen
+$passSecure = Convertto-SecureString $pass -AsPlainText -Force
